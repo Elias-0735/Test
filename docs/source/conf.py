feac83c4f1c3f677dc98046b17696f0dedd58f35
wyl <1411452414@qq.com>
2026-08-18 14:37:04 +0800

    first commit

---
+++ docs/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'test'
+copyright = '2026, test'
+author = 'test'
+release = 'v0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# ========= sphinx‑intl多语言配置 =========
+locale_dirs = ['../locale/']   # locale和source平级，../向上跳一级
+gettext_compact = False
+language = "en"   # ⚠️基准源码语言改为英文 en
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+html_copy_source = False
+html_show_sourcelink = False
+html_show_sphinx = False
+html_secnumber_suffix = ' '
---
+++ docs/source/index.md
@@ -0,0 +1,4 @@
+test documentation
+==================
+
+The AGIBOT D1 Edu & Ultra quadruped robot features 12 degrees of freedom and supports a wide variety of locomotion gaits and stunt maneuvers. The product also provides an open motion-control algorithm development SDK and communication protocol, enabling users to perform secondary development as needed.
